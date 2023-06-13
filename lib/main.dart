046d7405d6f486ba1e0da4217b97086ff3f98e1f
Subhas Sanghani <devdip1234@gmail.com>
2023-06-13 18:43:11 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+ import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:get_it/get_it.dart';
+
+import 'package:mathgame/src/ui/app/app.dart';
+import 'package:mathgame/src/ui/app/theme_provider.dart';
+import 'package:mathgame/src/ui/dashboard/dashboard_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kDebugMode) {
+    Animate.restartOnHotReload = true;
+  }
+
+  await Firebase.initializeApp(
+    // options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.instance;
+
+  // if (kDebugMode) {
+  //   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  //   await firebaseAnalytics.setAnalyticsCollectionEnabled(false);
+  // }
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  setupServiceLocator(sharedPreferences);
+  runApp(
+    MultiProvider(
+      providers: [
+        // Provider<SharedPreferences>(create: (context) => sharedPreferences),
+        ChangeNotifierProvider(
+          create: (context) =>
+              ThemeProvider(sharedPreferences: sharedPreferences),
+        ),
+        ChangeNotifierProvider<DashboardProvider>(
+          create: (context) => GetIt.I.get<DashboardProvider>(),
+        )
+      ],
+      child: MyApp(
+        // firebaseAnalytics: firebaseAnalytics,
+      ),
+    ),
+  );
+}
+
+setupServiceLocator(SharedPreferences sharedPreferences) {
+  GetIt.I.registerSingleton<DashboardProvider>(
+      DashboardProvider(preferences: sharedPreferences));
+}
